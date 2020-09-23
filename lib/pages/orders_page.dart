d7295949c74fbf1ab0ac463fd6108ea9a7b0d79b
arjunQ21 <arjunadhikari789@gmail.com>
2020-09-23 14:45:40 +0545

    added web

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:smooth_star_rating/smooth_star_rating.dart';
 import '../components/order.dart';
 
 class OrdersPage extends StatelessWidget {
